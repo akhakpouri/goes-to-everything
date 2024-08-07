f732f5611896d944887a26aa21122755476bc7fc
Ali Khakpouri <ali.khakpouri@gmail.com>
2024-08-07 12:32:59 -0400

    created more packages

--- example/greetings.go
+++ example/greetings.go
@@ -5,11 +5,10 @@ import (
 	"fmt"
 )
 
-func Hello(name string) (string, error) {
+func SayHello(name string) (string, error) {
 	if name == "" {
 		return name, errors.New("name is empty")
 	}
 	message := fmt.Sprintf("hello %s", name)
 	return message, nil
-
 }
---
+++ hello/go.mod
@@ -0,0 +1,7 @@
+module example.com/hello
+
+go 1.22.6
+
+replace example/greetings => ../example
+
+require example/greetings v0.0.0-00010101000000-000000000000
