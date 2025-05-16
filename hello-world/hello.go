f69f18058d98e86c984b67a9d0e786100e78b2bd
Ali Khakpouri <ali.khakpouri@transunion.com>
2025-05-16 16:16:44 -0400

    updated mod

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,12 +1,5 @@
 package learning
 
-import "fmt"
-
 func SayHello(universe string) string {
 	return "hello, " + universe
-}
-
-func main() {
-	fmt.Println(SayHello("world"))
-	fmt.Println(SayHello("earth"))
 }
